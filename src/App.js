a1c2d86ae2e2845737e6c07016d42bf7a5809f0c
Fabrizio Ferorelli <fabrizio.ferorelli@gmail.com>
2019-11-05 13:21:17 +0100

    message

--- src/App.js
+++ src/App.js
@@ -2,25 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      names:[]
+    }
+   this.handleSubmit = this.handleSubmit.bind(this);  // oppure usare arrow function ----> handleSubmit
+  }
+
+handleSubmit(event){
+  event.preventDefault();
+  var name = this.refs.name.value;
+  console.log(`hai scritto ${name}`);   //ora che houn dato aggiorno lo stato
+  this.refs.name.value ="";             //pulisco
+  let names = this.state.names.slice();
+  names.push(name);
+  this.setState({names:names})
+}
+
+
+
+
+
+ render(){
+   let listaNomi = this.state.names.map((name,index) => {
+     return(
+          <li key = {index}> {name} </li>
+         )
+    });
+   
+   return(
+<div>
+  <h1>Foglio Iscrizioni</h1>
+  <form onSubmit = {this.handleSubmit} >
+    <input type ="text" placeholder="bho" ref="name" />
+    <input type ="submit" value="invia" />
+  </form>
+  <div>
+    <h2>Nomi già iscritti</h2>
+   <ol>{listaNomi}</ol> 
+  </div>
+</div> 
   );
+ }
 }
 
 export default App;
